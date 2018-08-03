36971ffb236ecfd12a18257bd349f2d97c7091ab
John Wade <johnw8017@gmail.com>
2018-08-03 05:57:09 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -34,8 +34,14 @@ module.exports = NodeHelper.create({
 	   gasset.push({name: gasset[i]['name'], ppg: gasset[i]['ppg'], dist: gasset[i]['dist']});
 	   // console.log(gasset[i]);
 		});
-		
-       console.log(gasset);
+	// this is what I get back........close but can't quite figure out how to send them back as single sets
+	// ie name: name, ppg: price, dist: distance as seperate objects
+	    // here is my return
+	   console.log(gasset);  
+	    //[ { name: '1. Kwik Fill #372. Express Mart #3533. Byrne Dairy #664. Express Mart #3225. Sues Pine Valley Busymart6. Speedway #7749 (discount Available)7. Express Mart #3708. Byrne Dairy #19. Dandy Mini Mart #6310. 7-eleven #35122',
+    //ppg: '$3.099$2.959$2.949$2.959$3.349$3.159$2.959$2.949$2.959$3.099',
+    //dist: '1.68 miles2.09 miles2.23 miles2.49 miles2.76 miles2.92 miles3.09 miles3.31 miles4.25 miles4.36 miles' },
+      
 	   self.sendSocketNotification("GAS_RESULT", gasset);
       })
      .catch((err) => {
