c980bb087a87ada0c4c889db8ef8bab928bb427b
John Wade <johnw8017@gmail.com>
2018-08-30 20:24:54 -0400

    Update to fix names

--- node_helper.js
+++ node_helper.js
@@ -23,17 +23,18 @@ module.exports = NodeHelper.create({
 
                 $('.stations-list tbody tr').each(function(i, elem) {
                     const href = decodeURI($(elem).find('.name a').attr('href')).split('=');
+					console.log(href);
                     const address = href[href.length - 1];
-					const link = decodeURI($(elem).find('.name a').attr('href'));
+					
 
                     const gaslist = {
-                        name: $(elem).find('td .name').text().replace(/\$.*/, ""),
+                        name: $(elem).find('.name a').text(),
                         ppg: $(elem).find('.ppg').text().replace(/[\n\t\r]/g, ""),
                         dist: $(elem).find('.dist').text().replace(/[\n\t\r]/g, ""),
                         address,
-						link
+						//link
                     };
-                    
+                    console.log(gaslist);
                     gasset.push(gaslist);
 					
                 });
