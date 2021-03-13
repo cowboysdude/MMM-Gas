0de62f7ce74651d99ae37680dccb95a3a3ae15e6
John Wade <johnw8017@gmail.com>
2021-03-12 20:34:20 -0500

    Updated 3/12/2021

--- MMM-Gas.js
+++ MMM-Gas.js
@@ -7,39 +7,29 @@
 Module.register("MMM-Gas", {
 
     defaults: {
-        updateInterval: 12 * 60 * 60 * 1000,
+        updateInterval: 60 * 1000,
         zip: "14904",
         items: 10,
-        typeGas: "",				//premium, mid-grade, diesel, or blank for regular
-        sortBy: "distance",	//distance, price
-		distance: true
-    },
-
-    voice: {
-        mode: 'GAS',
-        sentences: [
-            'HIDE GAS',
-            'SHOW GAS'
-        ]
+        typeGas: "regular", //premium, mid-grade, diesel, or blank for regular
+        sortBy: "distance", //distance, price
+        distance: true
     },
 
     getStyles: function() {
-        if (this.config.distance != false){
-		return ["MMM-Gas.css"]
+        if (this.config.distance != false) {
+            return ["MMM-Gas.css"]
         } else {
-		return  ["MMM-Gas1.css"]
-		}
+            return ["MMM-Gas1.css"]
+        }
     },
-	getScripts: function(){
-		return ["moment.js"]
-	},
+    getScripts: function() {
+        return ["moment.js"]
+    },
 
     // Define start sequence.
     start: function() {
-        Log.info("Starting module: " + this.name);
         this.sendSocketNotification('CONFIG', this.config);
-        this.loaded = true;
-        this.getGAS();
+        Log.info("Starting module: " + this.name);
         this.gas = {};
         this.scheduleUpdate();
     },
@@ -73,13 +63,13 @@ Module.register("MMM-Gas", {
         weatherTable.appendChild(xFCRow);
 
 
-        if (this.config.distance != false){
-        var bjumpy = document.createElement("th");
-        bjumpy.setAttribute("style", "text-align:center");
-        bjumpy.classList.add("xsmall");
-        bjumpy.innerHTML = "Distance";
-        xFCRow.appendChild(bjumpy);
-        weatherTable.appendChild(xFCRow);
+        if (this.config.distance != false) {
+            var bjumpy = document.createElement("th");
+            bjumpy.setAttribute("style", "text-align:center");
+            bjumpy.classList.add("xsmall");
+            bjumpy.innerHTML = "Distance";
+            xFCRow.appendChild(bjumpy);
+            weatherTable.appendChild(xFCRow);
         }
 
         var gas = this.gas;
@@ -100,8 +90,8 @@ Module.register("MMM-Gas", {
             TDrow.classList.add("xsmall", "bright");
 
             var td2 = document.createElement("td");
-            td2.classList.add("align", "CellWithComment","yellow");
-            td2.innerHTML = storeslist + "<span class='CellComment'><u><font color=#F0F8FF><b>" + storeslist + "</font></b></u><BR><BR>" + gas.address + "<br> Updated: "+up+"</span>";
+            td2.classList.add("align", "CellWithComment", "yellow");
+            td2.innerHTML = storeslist + "<span class='CellComment'><u><font color=#F0F8FF><b>" + storeslist + "</font></b></u><BR><BR>" + gas.address + "</span>";
             TDrow.appendChild(td2);
             weatherTable.appendChild(TDrow);
 
@@ -110,29 +100,16 @@ Module.register("MMM-Gas", {
             TDrow.appendChild(td3);
             weatherTable.appendChild(TDrow);
 
-            if (this.config.distance != false){
-			var td5 = document.createElement("td");
-            td5.innerHTML = dist;
-            TDrow.appendChild(td5);
-            weatherTable.appendChild(TDrow);
-			}
+            if (this.config.distance != false) {
+                var td5 = document.createElement("td");
+                td5.innerHTML = dist;
+                TDrow.appendChild(td5);
+                weatherTable.appendChild(TDrow);
+            }
 
             top.appendChild(weatherTable);
             wrapper.appendChild(top);
-			}
-
-            //var doutput = moment().format("M.D.YYYY");
-            //var tinput = document.lastModified;
-            //var toutput = (moment(tinput.substring(10, 16), 'HH:mm').format('h:mm a'));
-		    //var x = this.config.updateInterval;
-		    //var y = moment.utc(x).format('mm');
-
-		//var mod = document.createElement("div");
-        //mod.classList.add("xxsmall","green");
-		//mod.setAttribute('style','text-align: center;');
-        //mod.innerHTML = "<font color=yellow>[</font>Updated: " +  doutput + " @ "+  toutput+"<font color=yellow>]</font>";
-        //wrapper.appendChild(mod);
-
+        }
 
         return wrapper;
 
@@ -141,35 +118,26 @@ Module.register("MMM-Gas", {
     processGAS: function(data) {
         this.today = data.Today;
         this.gas = data;
+        this.loaded = true;
     },
 
     scheduleUpdate: function() {
         setInterval(() => {
             this.getGAS();
-			console.log("Updating Gas Prices");
+            console.log("Getting Gas Prices");
         }, this.config.updateInterval);
-
-        this.getGAS(this.config.initialLoadDelay);
+        this.getGAS();
     },
 
     getGAS: function() {
+        console.log("GO get it!");
+        console.log(this.url);
         this.sendSocketNotification('GET_GAS');
     },
 
-     notificationReceived(notification, payload, sender) {
-        if (notification === 'ALL_MODULES_STARTED') {
-            this.sendNotification('REGISTER_VOICE_MODULE', this.voice);
-        } else if (notification === 'VOICE_GAS' && sender.name === 'MMM-voice') {
-            this.checkCommands(payload);
-        } else if (notification === 'VOICE_MODE_CHANGED' && sender.name === 'MMM-voice' && payload.old === this.voice.mode) {
-            this.help = false;
-            this.updateDom(300);
-        }
-    },
-
-
     socketNotificationReceived: function(notification, payload) {
         if (notification === "GAS_RESULT") {
+            console.log("RESULTS");
             this.processGAS(payload);
         }
         this.updateDom();
